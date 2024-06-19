bccd445f3c59b1c63ecbe59dd219599787ef8d3b
EspereJonathan <hei.espere@gmail.com>
2024-06-19 15:25:23 +0300

    fix: modify CalendarService

--- src/main/java/com/espere/pointing_system/service/CalendarService.java
+++ src/main/java/com/espere/pointing_system/service/CalendarService.java
@@ -1,23 +1,38 @@
 package com.espere.pointing_system.service;
 
-import com.espere.pointing_system.entity.Calendar;
-
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class CalendarService {
-    private final Calendar calendar;
+    private final Set<LocalDate> publicHolidays;
 
     public CalendarService() {
-        calendar = new Calendar();
-        calendar.setPublicHolidays(new ArrayList<>());
-        calendar.getPublicHolidays().add(LocalDate.of(2024, 6, 17));
-        calendar.getPublicHolidays().add(LocalDate.of(2024, 6, 25));
-        calendar.getPublicHolidays().add(LocalDate.of(2024, 6, 26));
+        publicHolidays = new HashSet<>();
+        publicHolidays.add(LocalDate.of(2024, 6, 17));
+        publicHolidays.add(LocalDate.of(2024, 6, 25));
+        publicHolidays.add(LocalDate.of(2024, 6, 26));
     }
 
     public boolean isPublicHoliday(LocalDate date) {
-        return calendar.getPublicHolidays().contains(date);
+        return publicHolidays.contains(date);
+    }
+
+    public List<LocalDate> getWorkDaysForEmployee(LocalDate startDate, LocalDate endDate, boolean isGuardian) {
+        List<LocalDate> workDays = new ArrayList<>();
+        LocalDate currentDate = startDate;
+
+        while (!currentDate.isAfter(endDate)) {
+            if (isGuardian || (currentDate.getDayOfWeek() != DayOfWeek.SATURDAY && currentDate.getDayOfWeek() != DayOfWeek.SUNDAY)) {
+                workDays.add(currentDate);
+            }
+            currentDate = currentDate.plusDays(1);
+        }
+
+        return workDays;
     }
 }
 
